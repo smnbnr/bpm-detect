0d1cfae7759115c4cffab4c92c2941f4a76b00fd
Simon Börner <boernersimon@googlemail.com>
2022-12-19 23:50:33 +0100

    fixed styling

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const button = document.querySelector("#tap-button");
 const bpmOutput = document.querySelector("#bpm-output");
+const resetButton = document.querySelector("#reset");
 
 let tapTimes = [];
 
-button.addEventListener("click", () => {
+const getBPM = () => {
   const currentTime = Date.now();
   tapTimes.push(currentTime);
 
-  // Calculate BPM if at least two taps have been registered
   if (tapTimes.length > 1) {
     const timeBetweenTaps =
       tapTimes[tapTimes.length - 1] - tapTimes[tapTimes.length - 2];
     const bpm = 60000 / timeBetweenTaps;
     bpmOutput.textContent = Math.round(bpm);
   }
+};
+
+button.addEventListener("click", getBPM);
+resetButton.addEventListener("click", () => {
+  bpmOutput.textContent = 0;
 });
